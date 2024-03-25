e5bbf284097bf8e0c0466324663d20027aca08ca
Tony law <lawtatfaitony@gmail.com>
2024-03-25 13:38:14 +0800

    testing for return a .ts file list from index.m3u8
     Target :  to  clean the .ts file exclude the index.m3u8 ts list files

--- MediaGuard_Cmake/Test/TestMath/src/main.cpp
+++ MediaGuard_Cmake/Test/TestMath/src/main.cpp
@@ -22,6 +22,59 @@ void TestHelloWorld()
 	HelloWorld();
 }
 
+/* testing for return a .ts file list from index.m3u8
+ * Target :  to  clean the .ts file exclude the index.m3u8 ts list files
+*/
+bool get_ts_list_file_from_index_m3u8(const std::string& path, std::vector<std::string>& vectorfiles)
+{
+    if (fs::exists(path))
+    {
+        std::ifstream file(path);
+
+        std::string line;
+
+        if (file.is_open()) {
+
+            while (std::getline(file, line)) {
+
+                if (!line.empty()) {
+                    // Remove trailing spaces
+                    while (std::isspace(line.back())) {
+                        line.pop_back();
+                    }
+
+                    if (line != "index.m3u8" && line[0] != '#' && line.substr(line.size() - 4) != ".tmp") {
+
+                        fs::path path(line);
+                        if (path.extension() == ".ts") {
+                            vectorfiles.push_back(line);
+                            //TEST LOG
+                            std::cout << "Add a file vectorfiles " << line << std::endl;
+                        }
+                        else {
+                            vectorfiles.push_back(line);
+                            //TEST LOG
+                            std::cout << "no .ts file contained,discard to add to ts file list" << line << std::endl;
+                        }
+                    }
+                }
+            }
+            file.close();
+        }
+        else {
+            std::cerr << "Unable to open file" << std::endl;
+            return 1;
+        }
+    }
+    else {
+        //no index.m3u8
+        std::cout << "no index.m3u8 file in app root" << std::endl;
+    }
+
+    // no any ts list or no index.m3u8 files
+    return false;
+}
+
 
 void TestMathLibrary()
 {
@@ -41,46 +94,16 @@ int main()
 	
 	//=================================
     /* testing for return a .ts file list from index.m3u8
-     * Target :  to  clean the .ts file exclude the index.m3u8 ts files
+     * Target :  to  clean the .ts file exclude the index.m3u8 ts list files
     */
-    std::ifstream file("index.m3u8");
-    std::vector<std::string> vectorfiles;
-    std::string line;
+    fs::path path = fs::current_path().append("index.m3u8");
+    std::vector<std::string> vec_ts_files_index;
+    bool ret = get_ts_list_file_from_index_m3u8(path.string(), vec_ts_files_index);
+    std::cout << "get_ts_list_file_from_index_m3u8 : "<< ret << " count = "<< vec_ts_files_index.size() << std::endl;
+    // delete ts file not in vec_ts_files_index
 
-    if (file.is_open()) {
-        while (std::getline(file, line)) {
-           
-            if (!line.empty()) {
-                // 去除尾部空格
-                while (std::isspace(line.back())) {
-                    line.pop_back();
-                }
 
-                if (line != "index.m3u8" && line[0] != '#' && line.substr(line.size() - 4) != ".tmp") {
-
-                    fs::path path(line); 
-                    if (path.extension() == ".ts") {
-                        vectorfiles.push_back(line);
-                        //TEST LOG
-                        std::cout << "Add a file vectorfiles " << line << std::endl;
-                    }
-                    else {
-                        vectorfiles.push_back(line);
-                        //TEST LOG
-                        std::cout << "no .ts file contained,discard to add to ts file list" << line << std::endl;
-                    }
-                   
-                }
-            }
-        }
-        file.close();
-    }
-    else {
-        std::cerr << "Unable to open file" << std::endl;
-        return 1;
-    }
-
-    // 处理lines中的每一个索引，例如下载或处理...
+    
 
     return 0;
      
