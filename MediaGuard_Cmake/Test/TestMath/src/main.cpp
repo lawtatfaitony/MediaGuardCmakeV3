c17c28d976365b512829b1fea416c99f72106f70
Tony law <lawtatfaitony@gmail.com>
2024-03-25 11:09:23 +0800

    testing for return a .ts  file list from index.m3u8
    Target :  to  clean the .ts file exclude the index.m3u8 ts files

--- MediaGuard_Cmake/Test/TestMath/src/main.cpp
+++ MediaGuard_Cmake/Test/TestMath/src/main.cpp
@@ -3,14 +3,18 @@
 #include <MathLibrary/MathLibrary.h>
 
 #include <iostream>  
-#include <filesystem> 
-
+#include <filesystem>  
+#include <fstream>
+#include <vector>
+#include <string>
+#include <thread>
+#include <mutex>
 
 //$(ProjectDir)include\ 其他包含目錄 加入這句
 
-#include "Time.h"
-#include <thread>
-#include <mutex>
+namespace fs = std::filesystem;
+
+#include "Time.h" 
 #include "ManagerController.h"
 
 void TestHelloWorld()
@@ -34,9 +38,51 @@ int main()
 	std::cout << "Hello World\n";
 	  
 	std::cout << "Hello test  not use" << std::endl;
-	 
-	  
+	
+	//=================================
 
-	return 0;
+    std::ifstream file("index.m3u8");
+    std::vector<std::string> vectorfiles;
+    std::string line;
+
+    if (file.is_open()) {
+        while (std::getline(file, line)) {
+           
+            if (!line.empty()) {
+                // 去除尾部空格
+                while (std::isspace(line.back())) {
+                    line.pop_back();
+                }
+
+                if (line != "index.m3u8" && line[0] != '#' && line.substr(line.size() - 4) != ".tmp") {
+
+                    fs::path path(line); 
+                    if (path.extension() == ".ts") {
+                        vectorfiles.push_back(line);
+                        //TEST LOG
+                        std::cout << "Add a file vectorfiles " << line << std::endl;
+                    }
+                    else {
+                        vectorfiles.push_back(line);
+                        //TEST LOG
+                        std::cout << "no .ts file contained,discard to add to ts file list" << line << std::endl;
+                    }
+                   
+                }
+            }
+        }
+        file.close();
+    }
+    else {
+        std::cerr << "Unable to open file" << std::endl;
+        return 1;
+    }
+
+    // 处理lines中的每一个索引，例如下载或处理...
+
+    return 0;
+     
 }
+
+
 
